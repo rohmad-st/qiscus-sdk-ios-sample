9349f0f7064dfceada378c180e69de29825c2a71
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2017-01-11 11:17:26 +0700

    convert to swift 3

--- SampleSDK/AppDelegate.swift
+++ SampleSDK/AppDelegate.swift
@@ -13,18 +13,29 @@ import Qiscus
 class AppDelegate: UIResponder, UIApplicationDelegate, QiscusConfigDelegate {
 
     var window: UIWindow?
+    var navController = UINavigationController()
+    var mainView = ViewController()
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let viewController = ViewController()
-        let navigationController = UINavigationController(rootViewController: viewController)
+        let mainView = ViewController()
+        let navigationController = UINavigationController(rootViewController: mainView)
         
         
-        Qiscus.setup(withAppId: "DRAGONFLY", userEmail: "e3@qiscus.com", userKey: "password", username: "Evan 3", avatarURL: "https://qiscuss3.s3.amazonaws.com/uploads/36976206a8b1fd2778938dbcd72b6624/qiscus-dp.png", delegate: self, secureURl: false)
+        mainView.showQiscusLoading()
+        Qiscus.setup(withAppId: "dragonfly", userEmail: "081212962117@qiscuswa.com", userKey: "26407cd298d88c131ff98d48201312c6", username: "Athaullah", avatarURL: "https://qiscuss3.s3.amazonaws.com/uploads/db5cbfe427dbeca6026d57c047074866/qiscus-dp.png", delegate: self)
         
-        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        //Chat view styling
+        Qiscus.style.color.leftBaloonColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1)
+        Qiscus.style.color.welcomeIconColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
+        Qiscus.style.color.leftBaloonTextColor = UIColor.white
+        Qiscus.style.color.rightBaloonColor = UIColor(red: 0.3, green: 0.3, blue: 0.3, alpha: 1)
+        Qiscus.style.color.rightBaloonTextColor = UIColor.white
+        Qiscus.setGradientChatNavigation(UIColor.black, bottomColor: UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1), tintColor: UIColor.white)
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         
@@ -32,36 +43,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate, QiscusConfigDelegate {
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
+    func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
+    func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
-    func applicationWillEnterForeground(application: UIApplication) {
+    func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
+    func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-    func applicationWillTerminate(application: UIApplication) {
+    func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
-    func qiscusFailToConnect(withMessage:String){
-        print("fail to connect")
+    // MARK: - QiscusConfigDelegate
+    func qiscusFailToConnect(_ withMessage:String){
+        print(withMessage)
+        mainView.dismissQiscusLoading()
     }
-    
     func qiscusConnected(){
-        print("success to connect")
+        print("Chat server connected")
+        mainView.dismissQiscusLoading()
     }
-
-
 }
 
--- SampleSDK/ViewController.swift
+++ SampleSDK/ViewController.swift
@@ -13,16 +13,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.title = "Qiscus SDK Sample"
-        
-        let button = UIButton(frame: CGRectMake(100,100,100,40))
-        button.setTitle("Chat", forState: .Normal)
-        button.backgroundColor = UIColor.redColor()
+        self.title = "Qiscus Test"
+        let button = UIButton(frame: CGRect(x: 50,y: 100,width: 200,height: 60))
+        button.setTitle("Test Qiscus Chat 2", for: UIControlState())
+        button.backgroundColor = UIColor.black
         self.view.addSubview(button)
-        
-        button.addTarget(self, action: #selector(ViewController.goToChat), forControlEvents: .TouchUpInside)
-        // Do any additional setup after loading the view, typically from a nib.
+        button.addTarget(self, action: #selector(ViewController.goToChat), for: .touchUpInside)
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,14 +28,10 @@ class ViewController: UIViewController {
     
     func goToChat(){
         print("go to chat")
-        
-        Qiscus.style.color.leftBaloonColor = UIColor.blueColor()
-        Qiscus.style.color.rightBaloonColor = UIColor.greenColor()
-        
-        Qiscus.chat(withUsers: ["e2@qiscus.com"], target: self)
+        Qiscus.chat(withUsers: ["085747455300@qiscuswa.com"] , target: self, title: "Chat 2", optionalDataCompletion: { optionalData in
+            print("optionalData from Example view: \(optionalData)")
+        })
         
     }
-
-
 }
 
